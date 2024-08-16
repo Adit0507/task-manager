d946d4c79d3f640ee87e3f0a5763f735a3a927c4
Adit0507 <adi05072002@gmail.com>
2024-08-16 20:41:09 +0530

    done with listing tasks

--- cmd/list.go
+++ cmd/list.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"task/db"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +12,22 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists all the tasks",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Somethin went wrong:", err.Error())
+			os.Exit(1)
+		}
+
+		if len(tasks) == 0{
+			fmt.Println("You have no tasks to complete 😐")
+			return
+		}
+
+		fmt.Println("You have the following tasks: ")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s", i +1, task.Value)
+		}
+	
 	},
 }
 
